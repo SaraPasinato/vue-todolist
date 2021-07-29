cbc2fa40066117d6c4838553179bfcebbda60fc9
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-29 14:45:32 +0200

    add boilerplate

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+ * Descrizione:
+     Rifare l'esercizio della to do list come fatto assieme in classe:
+      1. stampare in pagina un item per ogni elemento contenuto in un array
+      2. ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+      3. predisporre un input per aggiungere un nuovo item alla lista: ciccando su un pulsante, il testo digitato viene aggiunto alla lista.
+    ?BONUS 1: Fare un controllo di validazione per non aggiungere degli item vuoti.
+    ?BONUS 2: Dedicarsi alla parte grafica aggiungendo un tocco personale e gestendo anche il mobile.
+ */
+
+Vue.config.devtools=true;
+
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
